1f913b4ed5c49c0367c9a6c98a542ab79a46c13e
maciej pacek <maciekpacek1993@gmail.com>
2024-04-22 22:09:05 +0200

    Added new task: Remove element with array

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import mergeSortedArray.TaskMergeArray;
+import removeElement.TaskRemoveElement;
 
 import java.util.*;
 
@@ -6,9 +7,13 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums1 = {1,2,3,3,3,9};
         int[] nums2 = {2,5,6};
-        TaskMergeArray taskMergeArray = new TaskMergeArray(nums1, 3, nums2, 3);
-        TaskMergeArray.mergeTwoArrays(taskMergeArray);
+        //TaskMergeArray taskMergeArray = new TaskMergeArray(nums1, 3, nums2, 3);
+        //TaskMergeArray.mergeTwoArrays(taskMergeArray);
+
+        TaskRemoveElement taskRemoveElement = new TaskRemoveElement();
+        taskRemoveElement.removeElement(nums1, 3);
+
     }
 }
